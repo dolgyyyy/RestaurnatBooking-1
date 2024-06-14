ee47325b2046bbef52a0f51ba3be006bcf8b7611
heehoha <dolgyuk@gmail.com>
2024-06-14 12:53:27 +0900

    [feature] Step12-1. TestableScheduler 클래스 만들기

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -13,6 +13,14 @@ CUSTOMER_WITH_MAIL = Customer("Fake Name", "010-1234-5678", "test@test.com")
 UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
+class TestableBookingScheduler(BookingScheduler):
+    def __init__(self, capacity_per_hour, date_time: str):
+        super().__init__(capacity_per_hour)
+        self._date_time = date_time
+
+    def get_now(self):
+        return datetime.strptime(self._date_timem, "%Y/%m/%d %H:%M")
+
 class SundayBookingScheduler(BookingScheduler):
     def __init__(self, capacity_per_hour):
         super().__init__(capacity_per_hour)
