7026cc9074e8ce15e8050abf53950da7f55a8b7a
heehoha <dolgyuk@gmail.com>
2024-06-14 11:21:21 +0900

    [refactoring] Step3-1

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from booking_scheduler import BookingScheduler
 from schedule import Customer, Schedule
 
+NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+CUSTOMER = Customer("Fake name", "010-1234-5678")
+ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 
 class TestBookingScheduler(unittest.TestCase):
-
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
-        not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
-        customer = Customer("Fake name", "010-1234-5678")
-        schedule = Schedule(not_on_the_hour, 1, customer)
+        schedule = Schedule(NOT_ON_THE_HOUR, 1, CUSTOMER)
         booking_scheduler = BookingScheduler(3)
 
         # act and assert
@@ -21,9 +21,8 @@ class TestBookingScheduler(unittest.TestCase):
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         # arrange
-        on_the_hour = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
         customer = Customer("Fake name", "010-1234-5678")
-        schedule = Schedule(on_the_hour, 1, customer)
+        schedule = Schedule(ON_THE_HOUR, 1, customer)
         booking_scheduler = BookingScheduler(3)
 
         # act
