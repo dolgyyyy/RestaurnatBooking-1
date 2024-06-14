60c2aed4b32cbebb02fea4225622eec648df17de
heehoha <dolgyuk@gmail.com>
2024-06-14 11:24:38 +0900

    [refactoring] Step3-3. SetUp으로 중복코드 제거하기 2

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -14,28 +14,26 @@ CAPACITY_PER_HOUR = 3
 class TestBookingScheduler(unittest.TestCase):
 
     def setUp(self):
-        super().setUp()
+        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
         schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
         # act and assert
         with self.assertRaises(ValueError):
-            booking_scheduler.add_schedule(schedule)
+            self.booking_scheduler.add_schedule(schedule)
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         # arrange
         customer = Customer("Fake name", "010-1234-5678")
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
-        booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
         # act
-        booking_scheduler.add_schedule(schedule)
+        self.booking_scheduler.add_schedule(schedule)
 
         # assert
-        self.assertTrue(booking_scheduler.has_schedule(schedule))
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
 
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
