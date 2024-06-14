b975923de162364b6c4df56b841cf217fa692619
heehoha <dolgyuk@gmail.com>
2024-06-14 11:41:21 +0900

    [feature] Step8-2. Email이 없을 때, 이메일 발송 여부 테스트

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -2,7 +2,7 @@ import unittest
 from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
-from communication_test import TestableSmsSender
+from communication_test import TestableSmsSender, TestableMailSender
 from schedule import Customer, Schedule
 
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
@@ -76,7 +76,16 @@ class TestBookingScheduler(unittest.TestCase):
         self.assertTrue(self.testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
-        pass
+        # arrange
+        testable_mail_sender = TestableMailSender()
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.booking_scheduler.set_mail_sender(testable_mail_sender)
+
+        # act
+        self.booking_scheduler.add_schedule(schedule)
+
+        # assert
+        self.assertEqual(testable_mail_sender.get_count_send_mail_is_called(), 0)
 
     def test_이메일이_있는_경우에는_이메일_발송(self):
         pass
