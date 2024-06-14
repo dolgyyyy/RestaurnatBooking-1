46ec18ee4d79947a17ae6d9b509dcf29b87a051a
heehoha <dolgyuk@gmail.com>
2024-06-14 11:58:56 +0900

    [feature] Step11-7. 월요일 Mocking을 사용하는 테스트 케이스 만들기

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -123,7 +123,15 @@ class TestBookingScheduler(unittest.TestCase):
             self.fail()
 
     def test_현재날짜가_일요일이_아닌경우_예약가능(self):
-        pass
+        # arrange
+        self.booking_scheduler = MondayBookingScheduler(CAPACITY_PER_HOUR)
+
+        # act
+        new_schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL)
+        self.booking_scheduler.add_schedule(new_schedule)
+
+        # assert
+        self.assertTrue(self.booking_scheduler.has_schedule(new_schedule))
 
 
 if __name__ == '__main__':
